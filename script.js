20f54d31d036cbeb0dae7ca003dabf9e95ce8c66
callMeDrStrange <bryan.liow.zy@gmail.com>
2023-11-16 14:02:57 +0000

    Update UI

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-jQuery(function($) {
-    $(window).on('scroll', function() {
+jQuery(function ($) {
+    // Existing code for navbar scroll effect
+    $(window).on('scroll', function () {
         if ($(this).scrollTop() >= 200) {
             $('.navbar').addClass('fixed-top');
         } else if ($(this).scrollTop() == 0) {
@@ -7,18 +8,19 @@ jQuery(function($) {
         }
     });
 
+    // Existing code for adjusting the dropdown on different screen sizes
     function adjustNav() {
         var winWidth = $(window).width(),
             dropdown = $('.dropdown'),
             dropdownMenu = $('.dropdown-menu');
 
         if (winWidth >= 768) {
-            dropdown.on('mouseenter', function() {
+            dropdown.on('mouseenter', function () {
                 $(this).addClass('show')
                     .children(dropdownMenu).addClass('show');
             });
 
-            dropdown.on('mouseleave', function() {
+            dropdown.on('mouseleave', function () {
                 $(this).removeClass('show')
                     .children(dropdownMenu).removeClass('show');
             });
@@ -28,6 +30,18 @@ jQuery(function($) {
     }
 
     $(window).on('resize', adjustNav);
-
     adjustNav();
+
+    // Smooth scroll to content section
+    $('a[href^="#"]').on('click', function (e) {
+        var target = $(this.hash);
+        if (target.length) {
+            e.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 800, function () {
+                window.location.hash = target.selector;
+            });
+        }
+    });
 });
